98f49f44681a43c78b5f0bdfcf2a3695647cee09
Stanislav Shurko <stanislav.shurko@coffeecup.app>
2024-06-29 18:21:12 +0200

    Navigating between pages

--- nextjs-dashboard/app/ui/fonts.ts
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -1,4 +1,5 @@
-import { Inter, Roboto } from 'next/font/google';
+import { Inter, Roboto, Lusitana } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'], display: 'swap', });
 export const roboto = Roboto({ weight: ['400', '700'], subsets: ['latin'], display: 'swap', });
+export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'], display: 'swap'});
