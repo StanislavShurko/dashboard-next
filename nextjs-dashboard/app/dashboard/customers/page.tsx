4db908d8c8a012ff85b0999187b289fc6e366455
Stanislav Shurko <stanislav.shurko@coffeecup.app>
2024-07-01 21:25:01 +0200

    Customers page

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,3 +1,23 @@
-export default function Page() {
-    return <p>Customers Page</p>;
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import Table from "@/app/ui/customers/table";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
+
+export default async function Page({ searchParams }: {
+    searchParams?: {
+        query?: string;
+        page?: string;
+    }
+}) {
+
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
+
+    const customers = await fetchFilteredCustomers(query);
+
+    return <div className="w-full">
+        <Suspense fallback={<InvoicesTableSkeleton />}>
+            <Table customers={customers} />
+        </Suspense>
+    </div>
 }
